7aebf3dcd1337aba00838d9ac7540c47107e29b9
wxson <wxson@126.com>
2022-04-06 20:51:25 +0800

    beta

--- commlib/src/main/java/com/wxson/commlib/MyApp.kt
+++ commlib/src/main/java/com/wxson/commlib/MyApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import cn.wch.ch34xuartdriver.CH34xUARTDriver
 
 class MyApp : Application() {
-    // 需要将CH34x的驱动类写在APP类下面，使得帮助类的生命周期与整个应用程序的生命周期是相同的
+    // 需要将CH34x的驱动类CH34xUARTDriver类的对象创建在 Application 类下，以保证在应用有多个
+    //Activity 切换时均能进行串口收发
     lateinit var driver: CH34xUARTDriver
 }
